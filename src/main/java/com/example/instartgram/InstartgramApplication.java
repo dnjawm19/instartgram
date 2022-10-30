c33918fde5f9ebb61f77dd7feae7b660762ee037
jeongsoo <dnjawm19@naver.com>
2022-10-31 00:16:06 +0900

    Initial commit

---
+++ src/main/java/com/example/instartgram/InstartgramApplication.java
@@ -0,0 +1,13 @@
+package com.example.instartgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstartgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstartgramApplication.class, args);
+    }
+
+}
